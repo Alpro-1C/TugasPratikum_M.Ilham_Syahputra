cd45987c555c49caf3f1d770b77ad7de2e23b01b
Kiraa30 <148196450+Kiraa30@users.noreply.github.com>
2023-11-30 14:31:37 +0700

    Add files via upload

---
+++ Tugas Pertemuan 1/ilham.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    for (int i = 0; i < 50; i++){
+        cout << "-";
+    }
+        cout<<"\nNama : M. Ilham Syahputra"<<endl;
+        cout<<"NPM : 2310631170027"<<endl;
+        cout<<"Kelas : C - Informatika"<<endl;
+        cout<<"Cita - Cita : Kerja WFA"<<endl;
+        cout<<"Motivasi Masuk Informatika : WFA with my waifu"<<endl;
+
+    for (int i = 0; i < 50; i++){
+        cout << "-";
+    }
+return 0;
+}
